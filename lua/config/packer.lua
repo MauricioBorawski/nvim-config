f4452d37d4d1a7c795ada156cecb034c4e8ff062
Mauricio Borawski <mauricio.borawski@gmail.com>
2023-07-26 17:29:08 -0300

    Initial Setup

---
+++ lua/config/packer.lua
@@ -0,0 +1,65 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd.packadd('packer.nvim')
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
+
+    use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+	use { "catppuccin/nvim", as = "catppuccin" }
+
+	use({
+		"folke/trouble.nvim",
+		config = function()
+			require("trouble").setup {
+				icons = false,
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			}
+		end
+	})
+
+		use("theprimeagen/harpoon")
+		use("mbbill/undotree")
+		use("tpope/vim-fugitive")
+		use {
+			'VonHeikemen/lsp-zero.nvim',
+			branch = 'v2.x',
+			requires = {
+				-- LSP Support
+				{'neovim/nvim-lspconfig'},             -- Required
+				{                                      -- Optional
+				'williamboman/mason.nvim',
+				run = function()
+					pcall(vim.api.nvim_command, 'MasonUpdate')
+				end,
+			},
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	}
+
+	end)
+
