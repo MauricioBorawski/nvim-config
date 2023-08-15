b495dd4eb0dff941d2a2e5638655cdb514820865
Mauricio Borawski <mauricio.borawski@gmail.com>
2023-08-15 20:34:27 -0300

    Change remaps and set rose-pine as default theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,9 @@
 function SetTheme(color)
-	color = color or "catppuccin-latte"
+	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
---	vim.api.nvim_set_hl(0, "Normal", {bg = none})
---	vim.api.nvim_set_hl(0, "NormalFloat", {bg = none})
+	vim.api.nvim_set_hl(0, "Normal", {bg = none})
+	vim.api.nvim_set_hl(0, "NormalFloat", {bg = none})
 end
 
 SetTheme()
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -44,7 +44,3 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
-
