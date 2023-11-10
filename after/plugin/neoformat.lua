1c950a680dea30cd65b8c226529a76bb37eac2a6
Mauricio Borawski <mauricio.borawski@gmail.com>
2023-11-10 18:47:03 -0300

    Remove packer

--- after/plugin/neoformat.lua
+++
@@ -1,9 +0,0 @@
--- Basic config
-vim.g.neoformat_enabled_javascript = {'prettier'}
-vim.g.neoformat_autoformat = true
-vim.g.neoformat_basic_format_align = 1
-
--- Remap create remap for Neoformat
-
-vim.api.nvim_set_keymap('n', '<leader>f', ':Neoformat<CR>', { noremap = true, silent = true })
-
