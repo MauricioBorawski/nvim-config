caafea508c1a607c6e66a7d1f70d37daecff176e
Mauricio Borawski <mauricio.borawski@gmail.com>
2024-01-29 11:38:16 -0300

    Add glass style to terminal

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -26,3 +26,6 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
+
+vim.api.nvim_set_hl(0, "Normal", {bg = none})
+vim.api.nvim_set_hl(0, "NormalFloat", {bg = none})
--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,12 +1,12 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
-  { "rebelot/kanagawa.nvim" },
+  { "rebelot/kanagawa.nvim", name = "kanagawa" },
   { "catppuccin/nvim", name = "catppuccin" },
   {"sainnhe/gruvbox-material", name = "gruvbox-material"},
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "kanagawa",
     },
   },
 }
